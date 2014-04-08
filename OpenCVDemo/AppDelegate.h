9e216b8d49c6e583d753029de174fae0c0b1f9b0
apple <taagoomacmini@apple.local>
2014-04-08 11:13:45 +0800

    Initial Commit

---
+++ OpenCVDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenCVDemo
+//
+//  Created by apple on 14-4-8.
+//  Copyright (c) 2014年 Taagoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenCVDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OpenCVDemo
+//
+//  Created by apple on 14-4-8.
+//  Copyright (c) 2014年 Taagoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
